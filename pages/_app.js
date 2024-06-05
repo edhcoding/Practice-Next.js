baa9f7e7ae8a04c6863d09323c62cb5e70bb3647
eundonghyeok <mephisto7191@naver.com>
2024-06-05 15:00:29 +0900

    초기세팅

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "@/styles/globals.css";
+import "@/styles/global.css";
+// .. 사용해서 상대경로를 사용했는데 @쓰면 프로젝트 최상위 폴더를 기준으로 경로 쓸 수 있음(alias)
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
