90ce8ec4b63caa73bc0872f0cf0781031616713d
eundonghyeok <mephisto7191@naver.com>
2024-06-10 11:29:43 +0900

    폰트 적용하기

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,21 @@ import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/global.css";
 // 전체 css를 적용하려면 _app.js폴더에 css 임포트 하면 됨
 
+// import { Noto_Sans_KR } from "@next/font/google";
+
+// const notoSansKR = Noto_Sans_KR({
+//   weight: ["400", "700"],
+//   // 문자열로 굵기 지정해줘야 함
+//   subsets: ["korean"],
+//   // 서브셋은 폰트에서 영문, 한글 이런 식으로 사용할 글자들만 골라서 사용할 때 쓰는 거임
+//   // 일단 빈 배열을 작성함으로써 전부 다 사용하는 걸로 했습니다.
+//   // 만약에 영문만 사용하는 폰트라면 ['latin']과 같이 써 주면 됩니다.
+// });
+
 export default function App({ Component, pageProps }) {
   // component는 현재 렌더링되고 있는 페이지를 나타냄
   // pageProps는 해당 페이지에 전달되고있는 props를 나타냄
+
   return (
     <>
       <Head>
