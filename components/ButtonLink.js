f0eb50c69f89e718e31939804222531f3818dfab
eundonghyeok <mephisto7191@naver.com>
2024-06-12 01:36:33 +0900

    필요없는 파일 삭제

--- components/ButtonLink.js
+++
@@ -1,6 +0,0 @@
-import Link from "next/link";
-import styles from "./ButtonLink.module.css";
-
-export default function ButtonLink({ className = "", ...props }) {
-  return <Link className={`${styles.buttonLink} ${className}`} {...props} />;
-}
--- components/StarRating.js
+++
@@ -1,8 +0,0 @@
-const RATINGS = [1, 2, 3, 4, 5];
-
-export default function StarRating({ value = 1 }) {
-  // value는 starRating임 기본값은 별1개
-
-  return <span>{RATINGS.map((rating) => (value >= rating ? "★" : "✩"))}</span>;
-}
-6;
