05b6a375c892a052dd77781314177935f1b17d27
eundonghyeok <mephisto7191@naver.com>
2024-06-12 15:07:23 +0900

    리퀘스트 다루기

--- practice2-nextjs/pages/api/short-links/[id].js
+++ practice2-nextjs/pages/api/short-links/[id].js
@@ -1,3 +1,22 @@
 export default function handler(req, res) {
-  res.send("안녕 다이나믹 라우트");
+  // request 객체는 리퀘스트에 대한 정보를 가져올 때 사용함
+  // 다이나믹 라우트의 params값고 쿼리스트링을 가져올 수 있음 => res.send(req.query); (params, 쿼리스트링 같은 객체 안에 있음)
+  // GET http://localhost:3000/api/short-links/123?q=codeit => {"q": "codeit", "id": "123"}
+  // body 참조 가능 => req.body
+  // 리퀘스트로 오는 쿠키값을 확인가능
+  // 쿠기에는 간단한 사용자 정보, 로그인 하고난 후 인증정보 같은거 브라우저에 저장 하는 용도로
+  // 리퀘스트 할 때마다 보내 주는 식으로 활용 => req.cookies
+  // method 다뤄볼거임 => req.method
+  // method 마다 다른 처리 하고 싶다면 switch case문 활용!
+  switch (req.method) {
+    case "PATCH":
+      res.send("ShortLink 수정");
+      break;
+    case "GET":
+      res.send("ShortLink 조회");
+      break;
+    default:
+      res.send();
+      break;
+  }
 }
