055bdcff22a16834f383d58b5459a098c10fd9aa
eundonghyeok <mephisto7191@naver.com>
2024-06-13 18:10:00 +0900

    도큐먼트 수정, 삭제하기

--- practice2-nextjs/pages/api/short-links/handler.js
+++ practice2-nextjs/pages/api/short-links/handler.js
@@ -11,19 +11,12 @@ export default async function handler(req, res) {
   // 리퀘스트 할 때마다 보내 주는 식으로 활용 => req.cookies
   // method 다뤄볼거임 => req.method
   // method 마다 다른 처리 하고 싶다면 switch case문 활용!
-
   // id값으로 도큐먼트 조회해보자
   await dbConnect();
   const { id } = req.query;
 
   switch (req.method) {
     case "PATCH":
-      // PATCH리퀘스트로 리퀘스트 바디 보내면 도큐먼트 수정할 거임
-      // 수정은 findByIdAndUpdate 함수 사용하면 됨
-      // 첫 아규먼트로는 도큐먼트 id 넘겨주고
-      // 두번째 아규먼트로는 업데이트할 데이터를 객체로 전달 (req.body)
-      // 세번째 아규먼트로를 안넣고 PATCH 리퀘스트 보내면 이전값이 같게 나옴 왜냐하면 findByIdAndUpdate함수의
-      // 리턴값은 수정하기 이전의 도큐먼트 값이기 때문에 세번째 아규먼트로 { new: true } 넣어줌 그럼 업데이트된 도큐먼트 출력됨
       const updateShortLink = await ShortLink.findByIdAndUpdate(id, req.body, {
         new: true,
       });
@@ -32,14 +25,6 @@ export default async function handler(req, res) {
     case "GET":
       const shortLink = await ShortLink.findById(id);
       res.send(shortLink);
-      break;
-    case "DELETE":
-      // 삭제할때는 findByIdAndDelete함수를 사용하고 인수로 id값을 넣어줌
-      // 리스폰스로는 빈 값을 넣어 줌
-      // 여기서 하나만 더 수정하자면 보통 데이터를 삭제하면 상태코드로 204 No Content를 보내주는데
-      // 그래서 status 함수로 204를 보내줌
-      await ShortLink.findByIdAndDelete(id);
-      res.status(204).send();
       break;
     default:
       res.send();
