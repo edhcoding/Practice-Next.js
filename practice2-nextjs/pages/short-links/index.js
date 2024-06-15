565f51029a4c1c6ebaaa15041413093876281c19
eundonghyeok <mephisto7191@naver.com>
2024-06-15 18:39:46 +0900

    짧은 주소 목록 보여주기

--- practice2-nextjs/pages/short-links/index.js
+++ practice2-nextjs/pages/short-links/index.js
@@ -1,13 +1,28 @@
-import Head from 'next/head';
-import Link from '@/components/Link';
-import Button from '@/components/Button';
-import styles from '@/styles/ShortLinkListPage.module.css';
-import ShortLinkList from '@/components/ShortLinkList';
+import Head from "next/head";
+import Link from "@/components/Link";
+import Button from "@/components/Button";
+import styles from "@/styles/ShortLinkListPage.module.css";
+import ShortLinkList from "@/components/ShortLinkList";
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
+
+export async function getServerSideProps(context) {
+  await dbConnect();
+  const shortLinks = await ShortLink.find();
+  return {
+    props: {
+      shortLinks: JSON.parse(JSON.stringify(shortLinks)),
+      // shortLinks는 JS객체이므로 JSON객체로 바꿔 줘야함
+      // 공식문서에 모델에 find메서드를 쓰면 return하는 클래스는 query라고 되어있음
+      // 이 값을 JSON으로 바꾸려면 메서드나 지원하지 않는 값은 직접 없애줘야함
+      // 먼저 JSON 문자열로 만들고 JSON.stringify(shortLinks)
+      // 다시 파싱하는거임 JSON.parse(JSON.stringify(shortLinks))
+    },
+  };
+}
 
 // 짧은 주소 목록을 확인하는 페이지
-export default function ShortLinkListPage() {
-  const shortLinks = [];
-
+export default function ShortLinkListPage(shortLinks) {
   return (
     <>
       <Head>
