d09eb4293f6078c25bce1405c976cf363ba81efd
eundonghyeok <mephisto7191@naver.com>
2024-06-16 00:52:21 +0900

    QRCode 새로 만들기

--- practice2-nextjs/pages/qrcodes/new.js
+++ practice2-nextjs/pages/qrcodes/new.js
@@ -1,9 +1,17 @@
 import Head from 'next/head';
-import styles from '@/styles/QRCodeCreatePage.module.css';
+import { useRouter } from 'next/router';
 import QRCodeForm from '@/components/QRCodeForm';
+import styles from '@/styles/QRCodeCreatePage.module.css';
+import axios from '@/lib/axios';
 
-// QR코드를 생성하는 페이지
 export default function QRCodeCreatePage() {
+  const router = useRouter();
+
+  async function handleSubmit(values) {
+    await axios.post('/qrcodes', values);
+    router.push('/qrcodes');
+  }
+
   return (
     <>
       <Head>
@@ -11,7 +19,7 @@ export default function QRCodeCreatePage() {
       </Head>
       <div className={styles.page}>
         <h1 className={styles.title}>새 QRCode 추가</h1>
-        <QRCodeForm />
+        <QRCodeForm onSubmit={handleSubmit} />
       </div>
     </>
   );
