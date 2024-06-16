99bebd618a1dcfda6c5162190af28bcf86dacb28
eundonghyeok <mephisto7191@naver.com>
2024-06-16 19:23:43 +0900

    tailwind 가지고 놀아보기

--- tailwind-nextjs/pages/index.tsx
+++ tailwind-nextjs/pages/index.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <section className="flex h-[250px] w-full flex-row items-center justify-between bg-neutral-700">
+      <div className="flex flex-col gap-4">
+        <div className="h-4 w-[200px] bg-white"></div>
+        <div className="h-4 w-[200px] bg-white"></div>
+        <div className="h-4 w-[200px] bg-white"></div>
+        <div className="h-4 w-[200px] bg-white"></div>
+        <div className="h-4 w-[200px] bg-white"></div>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-3">
+        <div className="h-32 w-32 rounded-full border-2 border-black bg-red-500 transition hover:bg-red-300"></div>
+        <div className="h-32 w-32 rounded-full border-2 border-black bg-yellow-500 transition hover:bg-red-300"></div>
+        <div className="h-32 w-32 rounded-full border-2 border-black bg-green-500 transition hover:bg-red-300"></div>
+      </div>
+      <div>|||</div>
+    </section>
+  );
 }
