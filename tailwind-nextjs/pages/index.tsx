924b3aad07dbc257d72efab94780f57d6c303399
eundonghyeok <mephisto7191@naver.com>
2024-06-23 14:16:52 +0900

    intellisense 작동오류 해결

--- tailwind-nextjs/pages/index.tsx
+++ tailwind-nextjs/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <div className="h-4 w-[200px] bg-white"></div>
         <div className="h-4 w-[200px] bg-white"></div>
         <div className="h-4 w-[200px] bg-white"></div>
+        <div className="h-2 w-[11px]">hi</div>
       </div>
       <div className="flex flex-row items-center justify-center gap-3">
         <div className="h-32 w-32 rounded-full border-2 border-black bg-red-500 transition hover:bg-red-300"></div>
